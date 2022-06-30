5d3c4c5b1025fc9360ae67e4f2685a836f08c41a
Nova Sultana <79368259+novasultana@users.noreply.github.com>
2022-06-30 11:04:28 +0600

    Add files via upload

---
+++ reverse_triangleNumber.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int rows,i,j,k;
+    printf("Enter Triangle of Numbers in Reverse Rows = ");
+    scanf("%d", &rows);
+
+    for ( i = 1; i <=rows ; i++)
+    {
+        for ( j = 1; j < i; j++)
+        {
+            printf(" ");
+        }
+        for ( k = i; k <= rows; k++)
+        {
+            printf("%d ", k);
+        }
+        printf("\n");
+    }
+
+
+}
+
+
+
+
