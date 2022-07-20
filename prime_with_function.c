45f681ed1487c35a44a24d95e24b825f2fa013bf
Nova Sultana <79368259+novasultana@users.noreply.github.com>
2022-07-21 00:41:09 +0600

    Add files via upload

---
+++ prime_with_function.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+CheckPrime(int x);
+int main()
+{
+    int x,c,i;
+    printf("Enter Number: ");
+    scanf("%d",&x);
+
+    CheckPrime(x);
+}
+
+int CheckPrime(int x)
+{
+    int c,i;
+ c=0;
+    for(i=2;i<x;i++)
+    {
+        if(x%i==0)
+        {
+            c++;
+            break;
+        }
+    }
+   if(c==0)
+   {
+       printf(" %d is Prime ",x);
+   }
+   else
+    printf("%d is not prime",x);
+
+}
