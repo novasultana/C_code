29ec7fcb43fbefb138f739b2fe4f2470fdf910c3
Nova Sultana <79368259+novasultana@users.noreply.github.com>
2022-07-21 13:34:23 +0600

    Add files via upload

---
+++ gcd_func.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int gcd(int a,int b);
+
+int main()
+{
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+    gcd(a,b);
+
+}
+
+int gcd(int a,int b)
+{
+    int x,y,t,gcd;
+    x=a;
+    y=b;
+    while(b!=0)
+    {
+        t=b;
+        b=a%b;
+        a=t;
+    }
+    gcd=a;
+
+    printf("GCD: %d \n",gcd);
+}
